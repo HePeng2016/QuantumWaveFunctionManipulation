cc60bce084052baa7f4160502fe4ade30f9142da
HePeng2016 <kevinhbxj@sina.com>
2018-05-30 17:45:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
  #include "WFM/WavafunctionManipulation.h"
-
- gsl_Type Context;
- char * buffer;
- buffer = (char *)malloc(sizeof(char)*1024);
- InitInvariantContext(&Context);
- std::vector< Coordinate > AtomN_o;
- std::vector<double> MO_o;
- InvariantDirection(bs,MOs[5],AtomN,MO_o,AtomN_o,&Context);
- FreeInvariantContext(&Context);
+int main()
+{
+  std::vector< Coordinate > AtomN;
+  std::vector< std::vector<double> >MOs;
+  std::vector<GTFtype>bs;
+  readfWFN("SMALL.WFN",AtomN,bs,MOs);
+  gsl_Type Context;
+  char * buffer;
+  buffer = (char *)malloc(sizeof(char)*1024);
+  InitInvariantContext(&Context);
+  std::vector< Coordinate > AtomN_o;
+  std::vector<double> MO_o;
+  InvariantDirection(bs,MOs[5],AtomN,MO_o,AtomN_o,&Context);
+  FreeInvariantContext(&Context);
+}
